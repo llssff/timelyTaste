c4b7eb76bf5ad9ab91afc11a71c163b929155e86
llssff <longlongsf@gmail.com>
2021-05-21 15:42:02 -0700

    Initial commit

---
+++ src/main/java/com/jlm/timelyTaste/entity/OrderItem.java
@@ -0,0 +1,42 @@
+package com.jlm.timelyTaste.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orderitem")
+public class OrderItem {
+
+    private static final long serialVersionUID = -2455760938054036364L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private int quantity;
+
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
